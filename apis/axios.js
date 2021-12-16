d43c9eb581e55427ed22bb687235facb4d79db9f
ariesastra <ariesastra@windowslive.com>
2021-12-16 11:51:16 +0700

    change service api to live server

--- apis/axios.js
+++ apis/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const server = axios.create({
-  baseURL: "http://localhost:5000" // Localhost
-  // baseURL: "https://tetangga-service.herokuapp.com/" // Production
+  // baseURL: "http://localhost:5000" // Localhost
+  baseURL: "https://tetangga-service.herokuapp.com/" // Production
 })
 
 export {
